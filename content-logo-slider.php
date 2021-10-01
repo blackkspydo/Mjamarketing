28f3bc2bbd9c425facd21bc3a4d100ba0a81aec9
blackkspydo <Upadhyaya13kishor@gmail.com>
2021-10-01 19:01:01 +0545

    final slick slider

---
+++ content-logo-slider.php
@@ -0,0 +1,16 @@
+<div class="container-slider-bs">
+    <div class="text-container-slider">
+        <h2 class="section-title">A snapshot of our logo design portfolio</h2>
+        <p class="mt-4"">We proudly create brands and websites that stand the test of time. A little bit of love goes into everything we create, take a look at this selection of graphic design work from a range of our clients.</p>
+        </div>
+        <div class=" slick-bs">
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-2b.png' ?>" alt="Logo Design 01"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-active-lofts.png' ?>" alt="Logo Design 02"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-albermarle.png' ?>" alt="Logo Design 03"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-digital-workspace.png' ?>" alt="Logo Design 04"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-DMF.png' ?>" alt="Logo Design 05"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-FA.png' ?>" alt="Logo Design 06"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-MCK.png' ?>" alt="Logo Design 07"></div>
+        <div class="slide-bs"><img src="  <?php echo get_template_directory_uri() . '/assets/images/portfolio/logos/logo-MM.png' ?>" alt="Logo Design 08"></div>
+    </div>
+</div>
